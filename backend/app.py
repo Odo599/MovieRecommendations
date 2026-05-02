5153ebc58b3ceae9d2c5c204e52cabcf08c43610
Owen Lusis <owen00064@gmail.com>
2026-05-02 14:17:59 +1000

    add password storage with an sqlite db

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required 
+from flask_jwt_extended import JWTManager, create_access_token 
 from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import os
 
+from database import db, add_user, AddUserReturnStatus, login, LoginStatus
+
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET", "change-me")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+
+db.init_app(app)
 
 jwt = JWTManager(app)
 
@@ -15,26 +20,33 @@ CORS(app, origins=["*"], supports_credentials=True)
 
 limiter = Limiter(get_remote_address, app=app, default_limits=["100 per minute"])
 
+with app.app_context():
+    db.create_all()
+
 @app.route("/login", methods=["POST"])
-def login():
-    data = request.json
-    username = data.get("username")
-    password = data.get("password")
+@limiter.limit("10 per minute")
+def login_user():
+    email = request.form["email"]
+    password = request.form["password"]
 
-    if username == "test" and password == "test":
-        token = create_access_token(identity=username)
-        return jsonify(access_token=token)
+    status = login(email, password)
 
-    return jsonify({"msg": "Bad credentials"}), 401
+    if status == LoginStatus.UNAUTHORISED:
+        return jsonify({"msg": "Bad credentials"}), 401
 
-@app.route("/api/search")
-@jwt_required()
-@limiter.limit("20 per minute")
-def search_movies():
-    query = request.args.get("q")
+    token = create_access_token(identity=email)
+    return jsonify(access_token=token)
 
-    if not query:
-        return jsonify({"error": "Missing query"}), 400
+@app.route("/users/create", methods=["POST"])
+@limiter.limit("10 per minute")
+def user_create():
+    status = add_user(
+            request.form["username"],
+            request.form["email"],
+            request.form["password"]
+            )
 
-    return jsonify("Test response")
+    if status == AddUserReturnStatus.CONFLICT:
+        return jsonify({"msg": "username or email taken"}), 409
+    return "", 204
 
---
+++ backend/database.py
@@ -0,0 +1,83 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import or_
+from flask_sqlalchemy import SQLAlchemy
+from enum import Enum 
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+import time
+
+
+# setup db
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    phash: Mapped[str] = mapped_column(unique=True)
+
+    def __init__(self, username: str, email: str, phash: str) -> None:
+        self.username = username
+        self.email = email
+        self.phash = phash
+
+# setup password hasher
+ph = PasswordHasher()
+def check_hash(hash: str, password: str):
+    try:
+        ph.verify(hash, password)
+        return True
+    except VerifyMismatchError:
+        return False
+
+# functions and enums
+class AddUserReturnStatus(Enum):
+    SUCCESS = 204
+    CONFLICT = 409
+
+def add_user(username: str, email: str, password: str):
+    existing = db.session.execute(db
+                .select(User)
+                .where(
+                    or_(
+                        User.username == username,
+                        User.email == email
+                        )
+                )).scalar_one_or_none()
+
+    if existing != None:
+        return AddUserReturnStatus.CONFLICT
+
+    phash = ph.hash(password)
+    
+    user = User( 
+            username=username,
+            email=email,
+            phash=phash
+            )
+    db.session.add(user)
+    db.session.commit()
+
+class LoginStatus(Enum):
+    SUCCESS = 200
+    UNAUTHORISED = 401
+
+def login(email: str, password: str):
+    existing = db.session.execute(db
+        .select(User)
+        .where(
+            User.email == email
+        )).scalar_one_or_none()
+
+    if existing == None:
+        time.sleep(50/1000)
+        return LoginStatus.UNAUTHORISED
+
+    phash = existing.phash
+    success = check_hash(phash, password)
+    if success: return LoginStatus.SUCCESS
+    return LoginStatus.UNAUTHORISED
+
