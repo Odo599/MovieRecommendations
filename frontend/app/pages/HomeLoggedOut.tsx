7373d5f9d846734aeb98bf4f6d8dc05dc10e2c4d
Owen Lusis <owen00064@gmail.com>
2026-05-10 18:48:43 +1000

    add logged in and logged out home page views

---
+++ frontend/app/pages/HomeLoggedOut.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router";
+
+export default function HomeLoggedOut() {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="p-[20px] flex md:flex-row justify-end gap-4 ">
+                <p className="mr-auto">Movies</p>
+                <button onClick={() => navigate("/login")} className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700">
+                    Create Account
+                </button>
+                <button onClick={() => navigate("/create-account")} className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75">
+                    Login
+                </button>
+            </div>
+            <div className="flex flex-col items-center justify-center h-full text-xl">
+                <p>Log in or create an account to get started</p>
+            </div>
+        </>
+    );
+}
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,25 +1,19 @@
-import SearchBar from "~/components/SearchBar";
 import type { Route } from "./+types/home";
+import { useLocalStorage } from "usehooks-ts";
+import HomeLoggedIn from "~/pages/HomeLoggedIn";
+import HomeLoggedOut from "~/pages/HomeLoggedOut";
 
 export function meta({}: Route.MetaArgs) {
     return [{ title: "Movie Recommendations" }];
 }
 
 export default function Home() {
-    return (
-        <>
-            <div className="p-[20px] flex md:flex-row justify-end gap-4 ">
-                <p className="mr-auto">Movies</p>
-                <button className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700">
-                    Create Account
-                </button>
-                <button className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75">
-                    Login
-                </button>
-            </div>
-            <div className="flex flex-col items-center justify-center h-full">
-                <SearchBar />
-            </div>
-        </>
+    const [loggedIn] = useLocalStorage<boolean>(
+        "logged_in",
+        false
     );
+
+    if (!loggedIn) return <HomeLoggedOut />
+    else return <HomeLoggedIn/>
+
 }
