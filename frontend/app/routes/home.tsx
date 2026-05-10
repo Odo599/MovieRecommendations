7c9bc3a6fdf0dd41be2d650b2d753b55daa86b6f
Owen Lusis <owen00064@gmail.com>
2026-05-10 18:37:49 +1000

    create header on home screen

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -7,8 +7,19 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <SearchBar />
-        </div>
+        <>
+            <div className="p-[20px] flex md:flex-row justify-end gap-4 ">
+                <p className="mr-auto">Movies</p>
+                <button className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700">
+                    Create Account
+                </button>
+                <button className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75">
+                    Login
+                </button>
+            </div>
+            <div className="flex flex-col items-center justify-center h-full">
+                <SearchBar />
+            </div>
+        </>
     );
 }
