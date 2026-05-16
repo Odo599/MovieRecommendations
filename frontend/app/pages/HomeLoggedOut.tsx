3098133d74ee1338e221605ecaa02f6093a4a436
Owen Lusis <owen00064@gmail.com>
2026-05-17 08:33:44 +1000

    fix home page buttons

--- frontend/app/pages/HomeLoggedOut.tsx
+++ frontend/app/pages/HomeLoggedOut.tsx
@@ -7,10 +7,16 @@ export default function HomeLoggedOut() {
         <>
             <div className="p-[20px] flex md:flex-row justify-end gap-4 ">
                 <p className="mr-auto">Movies</p>
-                <button onClick={() => navigate("/login")} className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700">
+                <button
+                    onClick={() => navigate("/create-account")}
+                    className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                >
                     Create Account
                 </button>
-                <button onClick={() => navigate("/create-account")} className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75">
+                <button
+                    onClick={() => navigate("/login")}
+                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75"
+                >
                     Login
                 </button>
             </div>
