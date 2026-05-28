f34a0149b9b5598936695d7a63ba425e6f7dc853
Owen Lusis <owen00064@gmail.com>
2026-05-28 17:11:26 +1000

    add back icon to search and watchlist

--- frontend/app/components/HeaderText.tsx
+++ frontend/app/components/HeaderText.tsx
@@ -1,5 +1,16 @@
-export default function HeaderText({text}: {text: string}) {
+import { Link } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function HeaderText({ text }: { text: string }) {
     return (
-        <div className="text-3xl text-center m-2 pt-1">{text}</div>
-    )
+        <div className="flex items-center px-6">
+            <div className="flex-1">
+                <Link to={".."}>
+                    <FontAwesomeIcon icon={["fas", "arrow-left"]} width={16}/>
+                </Link>
+            </div>
+            <div className="text-3xl m-2 pt-1">{text}</div>
+            <div className="flex-1" />
+        </div>
+    );
 }
