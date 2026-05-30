1fa4d6b656b86d8b127e8e6664aa4432e19eaf8d
Owen Lusis <owen00064@gmail.com>
2026-05-30 15:05:21 +1000

    remove old testing page

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,7 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("test", "routes/Test.tsx"),
     route("login", "routes/Login.tsx"),
     route("create-account", "routes/CreateAccount.tsx"),
     route("logout", "routes/Logout.tsx"),
