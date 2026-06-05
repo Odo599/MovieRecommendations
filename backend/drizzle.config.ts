80329347a9dbe4cff70cbd88649e989aa5328036
Owen Lusis <owen00064@gmail.com>
2026-06-05 17:49:14 +1000

    add a migrations container

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,14 @@
-import 'dotenv/config';
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env"})
 import { defineConfig } from 'drizzle-kit';
+
+const POSTGRES_URI = `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/postgres`
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: POSTGRES_URI,
   },
 });
